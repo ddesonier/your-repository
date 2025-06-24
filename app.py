1ebd66d6947acb867d1eab2053b81d1d0650656c
Damon DeSonier <ddesonier@microsoft.com>
2025-06-24 12:23:09 -0400

    all

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 import streamlit as st
+from streamlit_msal import Msal
+from dotenv import load_dotenv
 
-def login_screen():
-    st.header("This app is private.")
-    st.subheader("Please log in.")
-    st.button("Log in with Microsoft", on_click=st.login)
+load_dotenv()
 
-if not st.user.is_logged_in:
-    login_screen()
+client_id = os.getenv('client_id')
+tenant_id = os.getenv('tenant_id')
+
+auth_data = Msal.initialize(
+    client_id=client_id,
+    authority=f"https://login.microsoftonline.com/{tenant_id}",
+    scopes=["User.Read"]
+)
+
+if st.button("Sign in"):
+    Msal.sign_in()
+
+if auth_data:
+    st.write("Success You are signed in")
+    st.json(auth_data)
 else:
-    st.header(f"Welcome, {st.user.name}!")
-    st.button("Log out", on_click=st.logout)
+    st.write("You are not signed in")
+if st.button("Sign out"):
+    Msal.sign_out()
+    st.write("You have signed out")
